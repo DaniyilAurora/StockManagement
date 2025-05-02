5135a372cca2733238968ce83c845175d5c84439
Daniyil Preen <daniyil.aurora@gmail.com>
2025-05-02 19:45:03 +0200

    Added database, creates database when launched first time

--- app.py
+++ app.py
@@ -1,12 +1,19 @@
 from flask import Flask
+from database import Database
 
 
 class App():
     # When app is initialised
     def __init__(self):
         self.app = Flask(__name__)
+        self.setup_database()
         self.setup_routes()
 
+    # Setup database for the app
+    def setup_database(self):
+        database = Database()
+
+    # Setup routes for the app
     def setup_routes(self):
         @self.app.route("/")
         def home():
---
+++ database.py
@@ -0,0 +1,61 @@
+import sqlite3
+
+
+class Database():
+    # Initialise database
+    def __init__(self):
+        self.connection = sqlite3.connect("db.db")
+        self.cursor = self.connection.cursor()
+        
+        # Ensure tables exist
+        if self.is_first_launch():
+            self.create_tables()
+
+    def create_tables(self):
+        # Create tables in the database
+
+        # Create "stocks" table
+        self.cursor.execute("""
+                CREATE TABLE stocks(
+                    id INTEGER PRIMARY KEY,
+                    name VARCHAR(255),
+                    image_path VARCHAR(255),
+                    is_in_stock BOOLEAN
+                );
+        """)
+
+        self.connection.commit()
+
+        # Create "accounts" table
+        self.cursor.execute("""
+                CREATE TABLE accounts(
+                    id INTEGER PRIMARY KEY,
+                    username VARCHAR(255),
+                    password BLOB
+                );
+        """)
+        
+        self.connection.commit()
+
+        # Create "sessions" table
+        self.cursor.execute("""
+                CREATE TABLE sessions(
+                    id INTEGER PRIMARY KEY,
+                    user_id INTEGER,
+                    token TEXT,
+                    expires_at DATETIME
+                );
+        """)
+
+        self.connection.commit()
+
+    def is_first_launch(self) -> bool:
+        # Check for first launch of an app (if table "stocks" exist)
+        self.cursor.execute("""
+            SELECT name FROM sqlite_master
+            WHERE type='table' AND name=?;
+        """, ("stocks",))
+
+        result = self.cursor.fetchone()
+
+        return result is None
